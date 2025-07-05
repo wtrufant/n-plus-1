4a28c41e87a47cb2ed6fd3eb40c45f0525cea9df
Will Trufant <will@trufant.net>
2025-07-05 11:34:47 -0500

    Create relevant directories if missing, initial users backup

--- backup.sh
+++ backup.sh
@@ -5,8 +5,9 @@
 
 # You can't hide secrets from the future with math.  https://youtu.be/yVm8oZx9WSM
 
-#TODOS:
-# - Logging
+#TODO:
+# Logging
+# tar user dir exclusions, by user.
 
 BASEDIR="$(dirname "$0")"
 
@@ -24,28 +25,30 @@ MAX_M=$((MAX_M * 30 - 1))
 
 function backups() {
 
-	# Clean old dailies every day.
+	# Clean old dailies every day, if we have more than the max.
 	if [ "$(find "${BASEDIR}/$1/daily" -type f -name "*.tgz" | wc -l)" -gt "${MAX_D}" ]; then
 		find "${BASEDIR}/$1/daily" -type f -name "*.tgz" -mtime "+${MAX_D}" -delete
 	fi
 
-	# Clean old weeklies on Sunday.
+	# Clean old weeklies on Sunday, if we have more than the max.
 	if [ "$(date +%u)" == "7" ]; then
+		if [ ! -d "${BASEDIR}/$1/weekly" ]; then mkdir "${BASEDIR}/$1/weekly"; fi
 		cp "${BASEDIR}/$1/daily/${BK_DATE}.tgz" "${BASEDIR}/$1/weekly/${BK_DATE}.tgz"
 		if [ "$(find "${BASEDIR}/$1/weekly" -type f -name "*.tgz" | wc -l)" -gt "${MAX_W}" ]; then
 			find "${BASEDIR}/$1/weekly" -type f -name "*.tgz" -mtime "+${MAX_W}" -delete
 		fi
 	fi
 
-	# Clean old montlies on the first of the month.
+	# Clean old montlies on the first of the month, if we have more than the max.
 	if [ "$(date +%d)" == "01" ]; then
+		if [ ! -d "${BASEDIR}/$1/monthly" ]; then mkdir "${BASEDIR}/$1/monthly"; fi
 		cp "${BASEDIR}/$1/daily/${BK_DATE}.tgz" "${BASEDIR}/$1/monthly/${BK_DATE}.tgz"
 		if [ "$(find "${BASEDIR}/$1/monthly" -type f -name "*.tgz" | wc -l)" -gt "${MAX_M}" ]; then
 			find "${BASEDIR}/$1/monthly" -type f -name "*.tgz" -mtime "+${MAX_M}" -delete
 		fi
 	fi
 
-	rclone sync "/backups/$1" "${REMOTE}:${BUCKET}/$1"
+	rclone sync "${BASEDIR}/$1" "${REMOTE}:${BUCKET}/$1"
 
 }
 
@@ -54,13 +57,16 @@ shopt -s dotglob
 
 
 #### MariaDB
+if [ ! -d "${BASEDIR}/mariadb/daily" ]; then mkdir -p "${BASEDIR}/mariadb/daily"; fi
 mariadb-backup --backup --target-dir="${BASEDIR}/mariadb/daily/${BK_DATE}" > /dev/null 2>&1 || { rm -rf "${BASEDIR:?}/mariadb/daily/${BK_DATE}"; echo "MariaDB Backup failed."; exit 1; }
-tar -C "${BASEDIR}/mariadb/daily/${BK_DATE}" -czf "${BASEDIR}/mariadb/daily/${BK_DATE}.tgz" .
+tar -C "${BASEDIR}/mariadb/daily/${BK_DATE}" -czf "${BASEDIR}/mariadb/daily/mariadb-${BK_DATE}.tgz" .
 rm -rf "${BASEDIR:?}/mariadb/daily/${BK_DATE}"
 backups mariadb
 
 
 #### System
+if [ ! -d "${BASEDIR}/system/daily" ]; then mkdir -p "${BASEDIR}/system/daily"; fi
+
 # Get a list of installed packages:
 if [ -f /usr/bin/dpkg ]; then dpkg -l | grep '^ii' > "/etc/pkgs-dpkg.txt"; fi
 if [ -f /usr/bin/flatpak ]; then flatpak list | sort > "/etc/pkgs-flatpak.txt"; fi
@@ -68,7 +74,8 @@ if [ -f /usr/bin/pacman ]; then pacman -Q > "/etc/pkgs-pacman.txt"; fi
 if [ -f /usr/bin/rpm ]; then rpm -qa | sort > "/etc/pkgs-rpm.txt"; fi
 if [ -f /usr/bin/snap ]; then snap list > "/etc/pkgs-snap.txt"; fi
 
-tar -C "/etc" -czf "${BASEDIR}/system/daily/${BK_DATE}.tgz" .
+tar -C "/etc" -czf "${BASEDIR}/system/daily/etc-${BK_DATE}.tgz" .
+# /usr/local/bin ?
 backups system
 
 
@@ -94,8 +101,14 @@ NC
 
 
 ##### Users ( set as array in .config file )
-# exclude: VMs, snap, .local/share/Steam, .cache
-
+if [ ! -d "${BASEDIR}/users/daily" ]; then mkdir -p "${BASEDIR}/users/daily"; fi
+# --exclude=pattern : VMs, snap, .local/share/Steam, .cache
+for U in "${USERS[@]}"; do
+	USERDIR="/${U}"
+	if [ "${U}" != "root" ]; then USERDIR="/home${USERDIR}"; fi
+	tar -C "${USERDIR}" --exclude='.gnupg/S.gpg-agent*' -czf "${BASEDIR}/users/daily/${U}-${BK_DATE}.tgz" .
+done
+backups users
 
 #### WWW
 
